b2ed7c42a31dd1e62f22520e038c94b998d34a08
FancyBirb <domenic.ba49@gmail.com>
2024-05-22 20:18:26 +0200

    added roxygen2 comment to callC.R and how one can call the c funktion hello without dyn.load()

--- R/callC.R
+++ R/callC.R
@@ -1,3 +1,13 @@
-callC <- function(){
+#' callC
+#'
+#' callC calls the c function hello that prints out hello world. This is only a demo
+#'
+#' @return None
+#'
+#' @example
+#' callC()
+#'
+#' @export
+callC <-function(){
 .C("hello")
 }
