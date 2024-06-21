3cc432a0913d8d089fcf8d916d5e8ceb82bb69cc
FancyBirb <domenic.ba49@gmail.com>
2024-06-21 11:13:36 +0200

    Test

--- R/callC.R
+++ R/callC.R
@@ -11,9 +11,7 @@
 EuclideanDT_inC<-function(binary_image,rows,cols,output){
 	rx <- matrix(0,rows,cols)
 	ry <- matrix(0,rows,cols)
+	output <- matrix(0,rows,cols)
 	result_output <- .C("EuclideanDistanceTransformation",as.integer(binary_image),as.integer(rows),as.integer(cols),as.integer(output),as.integer(rx),as.integer(ry))
 	return(matrix(result_output[[4]],rows,cols,byrow=TRUE))
 }
-#call_test_C <-function(){
-#	.C("test")
-#}
