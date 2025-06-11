058bc77d643cc68363cee3a8e85f523e7dbd8d7a
ktlenz <kilian.lenz@tutanota.com>
2025-06-11 15:40:49 +0200

    Implemented NaN handling and tests

--- R/gradient8.R
+++ R/gradient8.R
@@ -1,6 +1,6 @@
 #' wrap_gradient8
 #'
-#' This will make the Gradient8 funktion available to R from the libtotopotoolbox subdirectory
+#' This will make the Gradient8 function available to R from the libtotopotoolbox subdirectory
 #' 
 #' @param dem Input of type Spatrast from Terra
 #' @param use_mp Future feature that will allow to parallelize the code
@@ -11,13 +11,23 @@
 #' @export
 
 gradient8 <- function(dem,use_mp=0) {
+  
+    # Extract input data
+    d <- get_grid_data(dem)
     
-    d <- get_grid_data(dem) # Extract input data
-    output <- single(length(d$z)) #create output array
+    # Handle missing data
+    fill_value <- min(d$z, na.rm=T) - 999
+    log_nans <- is.na(d$z)
+    d$z[log_nans] <- fill_value
+    
+    # Compute gradient8 using libtopotoolbox
+    output <- single(length(d$z))
     result <- .C("wrap_gradient8",outputR=as.single(output),as.single(d$z),as.single(d$cellsize),as.integer(use_mp), as.integer(d$dims))$outputR
-
-    G <- dem # copy Spatrast
-    terra::values(G) <- result #update Spatrast values
+    result[log_nans] <- NaN
+    
+    # Store results as terra SpatRaster
+    G <- dem
+    terra::values(G) <- result
     
     return(G)
 }
--- tests/testthat/test-wrap_gradient8.R
+++ tests/testthat/test-wrap_gradient8.R
@@ -1,3 +1,7 @@
-test_that("gradient8 calculates the gradient of an hight map", {
-  expect_equal(2 * 2, 4)
+test_that("test-gradient8 loads the elev.tif from terra and produces sum of all gradients", {
+  load_rast<-terra::rast(system.file("ex/elev.tif",package="terra"))
+  load_rast<-terra::project(load_rast,"epsg:32632",res=90.0)
+  expect_no_message(identifyflats(load_rast))
+  expect_no_error(identifyflats(load_rast))
+  expect_equal(sum(values(gradient8(load_rast)),na.rm=T), 62791.5986)
 })
