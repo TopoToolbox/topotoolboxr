959ed4ea76244c69c1fe1be39005513e3e1c191a
ktlenz <kilian.lenz@tutanota.com>
2025-06-13 10:54:32 +0200

    Implemented test of unit conversion

--- tests/testthat/test-wrap_gradient8.R
+++ tests/testthat/test-wrap_gradient8.R
@@ -6,7 +6,11 @@ test_that("test-gradient8 loads reference data and creates own data to compare o
   # Transposition
   DEMm <- t(matrix(1, nrow=5, ncol=5)*c(5,2,1,3,4))*c(4,3,2,1,3)
   DEM <- terra::rast(DEMm, crs="EPSG:25833")
-  expect_identical(values(gradient8(DEM)),values(t(gradient8(t(DEM)))))
+  G <- gradient8(DEM)
+  expect_identical(values(G),values(t(gradient8(t(DEM)))))
+  # Unit conversion
+  expect_identical(round(values(G),8),round(tan(values(gradient8(DEM,'degree')) * pi/180),8))
+  expect_identical(round(values(G),8),round(tan(asin(values(gradient8(DEM,'sine')))),8))
   # Missing data
   DEM <- DEMm
   DEM[2,2] <- NaN
