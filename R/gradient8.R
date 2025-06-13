bf63be518463734cfbb3544a018d9ca7789bd544
ktlenz <kilian.lenz@tutanota.com>
2025-06-13 10:37:30 +0200

    Implemented missing value handling and updated tests

--- R/gradient8.R
+++ R/gradient8.R
@@ -31,19 +31,15 @@ gradient8 <- function(dem,unit='tangent',use_mp=0) {
     # Extract input data
     d <- get_grid_data(dem)
     
-    # Handle missing data
-    #fill_value <- NaN #min(d$z, na.rm=T) - 999
-    #log_nans <- is.na(d$z)
-    #d$z[log_nans] <- fill_value
-    
     # Compute gradient8 using libtopotoolbox
     output <- single(length(d$z))
     result <- .C("wrap_gradient8",
                  outputR=as.single(output),
                  as.single(d$z),as.single(d$cellsize),
                  as.integer(use_mp),
-                 as.integer(d$dims))$outputR
-    #result[log_nans] <- NaN
+                 as.integer(d$dims),
+                 NAOK=TRUE)$outputR
+    result[is.na(d$z)] <- NA
     
     # Unit conversion
     if (unit == 'degree'){
--- tests/testthat/test-wrap_gradient8.R
+++ tests/testthat/test-wrap_gradient8.R
@@ -1,9 +1,21 @@
-test_that("test-gradient8 loads the elev.tif from terra and produces sum of all gradients", {
-  #load_rast<-terra::rast(system.file("ex/elev.tif",package="terra"))
-  #load_rast<-terra::project(load_rast,"epsg:32632",res=90.0)
+test_that("test-gradient8 loads reference data and creates own data to compare outputs to known values.", {
+  # Plain test
+  DEM <- terra::rast(system.file("ex/elev.tif",package="terra"))
+  DEM <- terra::project(DEM,"EPSG:32632",res=90.0)
+  expect_equal(sum(values(gradient8(DEM)),na.rm=T), 11937.3066)
+  # Transposition
   DEMm <- t(matrix(1, nrow=5, ncol=5)*c(5,2,1,3,4))*c(4,3,2,1,3)
   DEM <- terra::rast(DEMm, crs="EPSG:25833")
-  expect_equal(sum(values(gradient8(DEM))), 107.3137090)
-  expect_no_message(identifyflats(DEM))
-  expect_no_error(identifyflats(DEM))
+  expect_identical(values(gradient8(DEM)),values(t(gradient8(t(DEM)))))
+  # Missing data
+  DEM <- DEMm
+  DEM[2,2] <- NaN
+  DEM <- terra::rast(DEM, crs="EPSG:25833")
+  G <- gradient8(DEM)
+  expect_true(is.na(sum(values(G))))
+  expect_equal(sum(values(G),na.rm=T), 103.091884)
+  # Output
+  expect_no_message(gradient8(DEM))
+  expect_no_error(gradient8(DEM))
+  expect_visible(gradient8(DEM))
 })
