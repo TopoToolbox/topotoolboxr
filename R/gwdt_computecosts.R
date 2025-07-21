08ba2f5af526cde55cbb7e49d98d9c8b22ad5e3d
ktlenz <kilian.lenz@tutanota.com>
2025-07-21 17:09:31 +0200

    Started implementing gwdt_computecosts (still buggy)

--- R/gwdt_computecosts.R
+++ R/gwdt_computecosts.R
@@ -3,7 +3,7 @@
 #' This function makes the libopotoolbox cost computation for the gray-weighted 
 #' distance transform available in R
 #' 
-#' @param flats 
+#' @param flats (terra::SpatRaster) 
 #' @param original_dem (terra::SpatRaster) Raw digital elevation model 
 #' @param filled_dem (terra::SpatRaster) Processed DEM
 #'
@@ -16,26 +16,26 @@ gwdt_computecosts <- function(flats, original_dem, filled_dem){
   fl <- get_grid_data(flats)
   dr <- get_grid_data(original_dem)
   df <- get_grid_data(filled_dem)
-  
-  # Check dims
+
+  # Check inputs
   if (!identical(fl$dims, dr$dims) || !identical(fl$dims, df$dims)) {
     stop("All input grids must have the same dimensions.")
   }
-  
+  # Range of values for flats
+
   # Compute costs using libtopotoolbox
-  costs <- single(length(dr$z))
-  conncompsR <- integer(length(dr$z))
+  outputs <- single(length(fl$z))
   results <- .C("wrap_gwdt_computecosts",
-                costsR=as.single(costs),conncompsR=as.integer(conncompsR),
+                costsR=as.single(outputs),conncompsR=as.integer(outputs),
                 as.single(fl$z),as.single(dr$z),as.single(df$z),
                 as.integer(fl$dims))
 
   # Write results into SpatRaster
   costs <- flats
-  terra::values(costs) <- results$costs
+  terra::values(costs) <- results$costsR
   comps <- flats
-  terra::values(comps) <- results$conncomps
-  
-  return(list("Costs" = costs,
-              "Conncomps" = comps))
+  terra::values(comps) <- results$conncompsR
+
+  return(list("costs" = costs,
+              "conncomps" = comps))
 }
--- src/wrap_gwdt_computecosts.c
+++ src/wrap_gwdt_computecosts.c
@@ -1,6 +1,5 @@
 #include <stddef.h>
 #include <stdint.h>
-
 #include <R.h>
 
 #include "topotoolbox.h"
@@ -9,21 +8,26 @@
 void wrap_gwdt_computecosts(float *costsR, int *conncompsR, int *flatsR,
                             float *original_demR, float *filled_demR, int *dimsR){
   
+  // Transform integers from R to ptrdiff_t
   ptrdiff_t dims [2]= {dimsR[0], dimsR[1]};
-  // Allocate memory for conncomps
+  
   ptrdiff_t *conncomps = R_Calloc(dims[0] * dims[1], ptrdiff_t);
   
-  // Fill conncomps from the int-valued conncompsR array
   for (ptrdiff_t j = 0; j < dims[1]; j++) {
     for (ptrdiff_t i = 0; i < dims[0]; i++) {
       conncomps[j * dims[0] + i] = conncompsR[j * dims[0] + i];
     }
   }
+
+  // Cost computation
+  gwdt_computecosts(costsR, conncomps, flatsR, original_demR, filled_demR, dims);
   
-  gwdt_computecosts(costsR, conncomps,
-                    flatsR, original_demR, filled_demR,
-                    dims);
-  
-  // Free the allocated memory
+  // Return conncomps as conncompsR
+  for (ptrdiff_t j = 0; j < dims[1]; j++) {
+    for (ptrdiff_t i = 0; i < dims[0]; i++) {
+      conncompsR[j * dims[0] + i] = (int)conncomps[j * dims[0] + i];
+    }
+  }
+
   R_Free(conncomps);
 }
